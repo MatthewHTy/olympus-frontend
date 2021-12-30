92e4cafd5bac9e89833e7d3222cb55cff37d8798
Matthew Ty <matthewty1220@gmail.com>
2021-12-29 18:54:51 -0700

    Changed the font of Square to Amadeus Regular

--- src/themes/global.js
+++ src/themes/global.js
@@ -33,7 +33,7 @@ const commonSettings = {
   direction: "ltr",
   typography: {
     fontSize: 16,
-    fontFamily: "Square",
+    fontFamily: "amadeusregular",
     h1: {
       fontSize: "3.3rem",
     },
